0b9208075b5cd700719e2762704e134fdd87f12e
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-11-15 17:40:48 +0800

    Create admin.bat

---
+++ admin.bat
@@ -0,0 +1,13 @@
+netsh winsock reset
+netsh int ip reset
+ipconfig /release
+ipconfig /renew
+ipconfig /flushdns
+arp -d *
+nbtstat -R
+nbtstat -RR
+ipconfig /registerdns
+echo please wait for wifi reconnect(1~2 min)
+echo please wait for dns resolve (uhh..maybe 15min?)
+echo please in browser press ctrl+f5 force refresh
+pause
