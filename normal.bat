6c8e9e4c298706625634cb47e6740c4114102a52
Wayne咏辰 <75261164+wayne931121@users.noreply.github.com>
2025-11-17 03:08:00 +0800

    Update normal.bat

--- normal.bat
+++ normal.bat
@@ -1,11 +1,15 @@
 ipconfig /release
 ipconfig /renew
 ipconfig /flushdns
+
 echo please wait for wifi reconnect (1~2 min)
-echo please in browser press ctrl+f5 force refresh
+
 echo you may need to clear ssl cache,
 echo   to clear ssl cache, there is a few way:
 echo   1. clear browser history
 echo   2. In chrome, type chrome://restart
 echo   3. use private page.
+
+echo please in browser press ctrl+f5 force refresh
+
 pause
